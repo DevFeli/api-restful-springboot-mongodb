7bf80ad45ff367d4438310591b8e3689f9f52ab8
felipe.reis <dev.felipereis@gmail.com>
2024-06-24 16:30:56 -0300

    created project

---
+++ src/main/java/com/api_restful_mongo/ApiRestfulMongoApplication.java
@@ -0,0 +1,13 @@
+package com.api_restful_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulMongoApplication.class, args);
+	}
+
+}
