9aaf404bdb24420b70970b73e3bf8ceef5aa7ba7
felipe.reis <dev.felipereis@gmail.com>
2024-06-25 11:07:59 -0300

    refactor posts with dto

--- src/main/java/com/api_restful_mongo/config/Instantiation.java
+++ src/main/java/com/api_restful_mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.api_restful_mongo.domain.Post;
 import com.api_restful_mongo.domain.User;
+import com.api_restful_mongo.dto.AuthorDTO;
 import com.api_restful_mongo.repository.PostRepository;
 import com.api_restful_mongo.repository.UserRepository;
 
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Viajando!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Feliz!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Viajando!",new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Feliz!",new AuthorDTO( maria));
+
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 	
