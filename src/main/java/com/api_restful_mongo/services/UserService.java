83c5ef421ff46d884f4988dc805c5d43db34055d
felipe.reis <dev.felipereis@gmail.com>
2024-06-25 08:50:15 -0300

    Excption id not found

--- src/main/java/com/api_restful_mongo/services/UserService.java
+++ src/main/java/com/api_restful_mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.api_restful_mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api_restful_mongo.domain.User;
 import com.api_restful_mongo.repository.UserRepository;
+import com.api_restful_mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
